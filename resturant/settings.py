767819853e00439cc792889277782a31d5bdb7c7
arokoolafemi <arokoolafemi@outlook.com>
2026-02-21 15:11:09 +0100

    debug_false

--- resturant/settings.py
+++ resturant/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r9b7f!qfu(ai$ucpp-gix+t7y-y4!#y2=hh(ye8^55uy!hyv4o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*", ".onrender.com"]
 
--- resturant/urls.py
+++ resturant/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -28,5 +29,7 @@ urlpatterns = [
     path ('payment/', include ('payment.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# Only serve static and media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
